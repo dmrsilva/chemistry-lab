ce9f1c7dee120023f170eace79fb542a93fbb17b
Daniel Silva <daniel.mascr@gmail.com>
2022-04-12 11:54:33 -0300

    Backend project created

---
+++ backend/src/main/java/com/school/chemistrylab/ChemistrylabApplication.java
@@ -0,0 +1,13 @@
+package com.school.chemistrylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistrylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemistrylabApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/school/chemistrylab/ChemistrylabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.chemistrylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistrylabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
